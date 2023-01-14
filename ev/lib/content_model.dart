539ca6ec1f38881b27c0561032f69fcd0f6c17e5
Nikita <gargnikita2003@gmail.com>
2023-01-14 13:10:53 +0530

    boarding_screen

--- ev/lib/content_model.dart
+++ ev/lib/content_model.dart
@@ -16,19 +16,19 @@ class UnbordingContent {
 List<UnbordingContent> contents = [
   UnbordingContent(
       image: 'assets/screen1.png',
-      bg: Color.fromARGB(255, 131, 248, 135),
+      bg: const Color.fromARGB(255, 131, 248, 135),
       description: 'Recieve our assistance and emergency services',
       title: "With our quickest services, "
           "we've got you covered. "),
   UnbordingContent(
       image: 'assets/screen2.png',
-      bg: Color.fromARGB(255, 248, 251, 101),
+      bg: const Color.fromARGB(255, 248, 251, 101),
       description: 'Now locating a charging station would be just a click away',
       title: "Locate the nearest charging  "
           "station "),
   UnbordingContent(
     image: 'assets/screen3.png',
-    bg: Color.fromARGB(255, 156, 230, 233),
+    bg: const Color.fromARGB(255, 156, 230, 233),
     title: "Book a hassle-free charging slot",
     description: 'Booking a slot as per your preferred time is made '
         "simple",
